5b750c831a6b7d3a58dbf1305ec4120f979ff2b9
Ramon Audain <raudain123@gmail.com>
2020-05-16 04:48:59 +0000

    Solution #3

--- main.js
+++ main.js
@@ -2,37 +2,40 @@
 // Vowels are the characters "a", "e", "i", "o" and "u".
 
 function vowels(str){
-  // // Solution #1
-  // const letters = new Set(["a", "e", "i", "o", "u"])
-  // let count = 0;
-
-  // for (let char of str.toLowerCase()){
-  //   if (letters.has(char)){
-  //     count++
-  //   }
-  // }
-
-  // return count;
-
-  
-  // Solution #2
+  // Solution #1
+  const letters = new Set(["a", "e", "i", "o", "u"]);
   let count = 0;
-  const letters = "aeiou"
 
   for (let char of str.toLowerCase()){
-    if(letters.includes(char)){
-      count++
+    if (letters.has(char)){
+      count++;
     }
   }
 
   return count;
 
-  // console.log(str.match(/[aeiou]/g).length)
   
-  //  ? 0 : str.match(/[aeiou]/g).length;
+  // Solution #2
+  let count = 0;
+  const letters = ["a", "e", "i", "o", "u"]; // or you can use a string
+
+  for (let char of str.toLowerCase()){
+    if(letters.includes(char)){
+      count++;
+    }
+  }
+
+  return count;
+
+
+  // Solution #3
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches === null ? 0 : matches.length;
+
 }
 
 
 // vowels("Hi There!");
-// vowels("Why do you ask?");
-vowels("Why?");
+vowels("Why do you ask?");
+// vowels("Why?");
